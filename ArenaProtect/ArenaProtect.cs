dc0f10d9d84773240f2640dbeceadf143dbce1df
AtiLion <thegrimpunisher1@gmail.com>
2017-06-12 00:35:43 +0200

    Added code

---
+++ ArenaProtect/ArenaProtect.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+
+namespace ArenaProtect
+{
+    public class ArenaProtect : RocketPlugin<Configuration>
+    {
+        private DateTime startTime = DateTime.Now;
+        private bool Finished = true;
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "protected", "You have been protected for {0} seconds by arena spawn protection!" },
+            { "unprotected", "You are no longer protected by arena spawn protection!" }
+        };
+
+        public override void LoadPlugin()
+        {
+            LevelManager.onLevelNumberUpdated += new LevelNumberUpdated(OnPlayersAmount);
+        }
+
+        private void OnPlayersAmount(int players)
+        {
+            if (players != Provider.clients.Count)
+                return;
+
+            foreach (ArenaPlayer player in LevelManager.arenaPlayers)
+            {
+                UnturnedPlayer uPlayer = UnturnedPlayer.FromSteamPlayer(player.steamPlayer);
+
+                if (Configuration.Instance.GodMode)
+                    uPlayer.GodMode = true;
+                if (Configuration.Instance.Vanish)
+                    uPlayer.VanishMode = true;
+
+                ChatManager.say(player.steamPlayer.playerID.steamID, Translate("protected", Configuration.Instance.Seconds), Configuration.Instance.color);
+            }
+            startTime = DateTime.Now;
+            Finished = false;
+        }
+
+        void Update()
+        {
+            if (Finished)
+                return;
+
+            if((DateTime.Now - startTime).TotalSeconds >= Configuration.Instance.Seconds)
+            {
+                foreach (ArenaPlayer player in LevelManager.arenaPlayers)
+                {
+                    UnturnedPlayer uPlayer = UnturnedPlayer.FromSteamPlayer(player.steamPlayer);
+
+                    uPlayer.GodMode = false;
+                    uPlayer.VanishMode = false;
+
+                    ChatManager.say(player.steamPlayer.playerID.steamID, Translate("unprotected"), Configuration.Instance.color);
+                }
+                Finished = true;
+            }
+        }
+    }
+}
---
+++ ArenaProtect/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+using UnityEngine;
+
+namespace ArenaProtect
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public bool Vanish;
+        public bool GodMode;
+        public int Seconds;
+        public Color color;
+
+        public void LoadDefaults()
+        {
+            Vanish = false;
+            GodMode = true;
+            Seconds = 10;
+            color = Color.yellow;
+        }
+    }
+}
